35a7bc358fd7d3a4d047b0709d1feeb9671f62d6
Rhys Evans <rhys.evans@ft.com>
2016-03-29 12:31:15 +0100

    handle rss and json for streams

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const get = require('./lib/get');
 const dynamos = require('./lib/dynamos');
 const health = require('./lib/health');
 const cache = require('./lib/cache');
-
+const extensionRX = /\.(?:json|rss)$/;
 let metrics;
 let useCache = false;
 let timeout;
@@ -13,14 +13,20 @@ let timeout;
 exports.health = health.check;
 
 exports.get = fromURL => {
+
 	if(useCache){
 		let cacheItem = cache.retrieve(fromURL);
 		if(cacheItem){
 			return Promise.resolve(cacheItem);
 		}
 	}
+	const extension = (extensionRX.exec(fromURL) || [''])[0];
+	if (extension) {
+		fromURL = fromURL.replace(extensionRX, '');
+	}
 
 	const dynamo = dynamos[active()];
+
 	return get({
 		dynamo: dynamo.instance,
 		table: dynamo.table,
@@ -32,13 +38,18 @@ exports.get = fromURL => {
 			// NB. This will still get cached by the next then because
 			// now this promise is not rejected anymore.
 			return {
-				fromURL,
-				toURL: fromURL,
+				fromURL: fromURL + extension,
+				toURL: fromURL + extension,
 				code: 100
 			};
 		}
 		return Promise.reject(err);
 	}).then(result => {
+		result = {
+			fromURL: result.fromURL + extension,
+			toURL: result.toURL + extension,
+			code: result.code
+		};
 		useCache && cache.store(fromURL, result);
 		return result;
 	});
